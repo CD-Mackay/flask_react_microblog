befaa011e11e89314434842304d3a0d271aed8b9
Connor Mackay <connor.mackay@gmail.com>
2023-01-27 14:01:25 -0500

    add user model

--- api/app/models.py
+++ api/app/models.py
@@ -6,3 +6,13 @@ class Post(db.Model):
 
   def __repr__(self):
     return '<Post {}>'.format(self.content)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
