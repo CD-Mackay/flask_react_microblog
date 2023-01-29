406c4a75adb155eeae857349e47c4c4d32113b2e
Connor Mackay <connor.mackay@gmail.com>
2023-01-29 18:52:35 -0500

    implement successful login

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 import time
 from app.forms import LoginForm
+from flask import flash, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -19,6 +20,10 @@ def get_posts():
 @app.route('/login',method=['POST'])
 def login():
     form = LoginForm
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return form
 
     
