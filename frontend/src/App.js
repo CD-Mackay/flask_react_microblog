2d75b72a642ba9541c98c2fc1ea379c7eefffa41
Connor Mackay <connor.mackay@gmail.com>
2023-02-01 13:45:23 -0500

    delete unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect} from 'react'
-import LoginForm from './Components/LoginForm';
-import UseToken from './Components/UseToken';
-import Header from './Components/Header';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import LoginForm from "./Components/LoginForm";
+import UseToken from "./Components/UseToken";
+import Header from "./Components/Header";
 
 function App() {
-
   const { token, removeToken, saveToken } = UseToken();
-  const [time, setTime] = useState(0)
-  const [posts, setPosts] = useState("")
-  const [showLogin, setShowLogin] = useState(false)
+  const [time, setTime] = useState(0);
+  const [posts, setPosts] = useState("");
+  const [showLogin, setShowLogin] = useState(false);
 
-  useEffect(() =>{
-    fetch('/time').then(res => res.json()).then(data => 
-      setTime(data.time)
-    )
-    fetch('/posts', {
+  useEffect(() => {
+    fetch("/time")
+      .then((res) => res.json())
+      .then((data) => setTime(data.time));
+    fetch("/posts", {
       headers: {
-        Authorization: 'Bearer ' + token
-      }
-    }).then(res => res.json()).then(data => 
-      setPosts(data.content)
-    )
-    
-  }, [])
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setPosts(data.content));
+  }, []);
 
   return (
     <div className="App">
       <Header token={token} removeToken={removeToken} />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {time}
-        </p>
-        {token && token!=="" &&token!== undefined && token !== null && 
-          <p>{posts}</p>
-          }
-                    {!showLogin && <button onClick={() => setShowLogin(true)}>Login?</button>}
-        {showLogin && <LoginForm saveToken={saveToken}/>}
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{time}</p>
+      {token && token !== "" && token !== undefined && token !== null && (
+        <p>{posts}</p>
+      )}
+      {!showLogin && <button onClick={() => setShowLogin(true)}>Login?</button>}
+      {showLogin && <LoginForm saveToken={saveToken} />}
     </div>
   );
 }
