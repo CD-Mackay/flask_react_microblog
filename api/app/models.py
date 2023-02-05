18c7409e2d547ab974e20071cc4c44df208af346
Connor Mackay <connor.mackay@gmail.com>
2023-02-05 18:38:09 -0500

    comment api models

--- api/app/models.py
+++ api/app/models.py
@@ -1,13 +1,13 @@
 from app import db
 
-class Post(db.Model):
+class Post(db.Model): ## Define Posts model
   id = db.Column(db.Integer, primary_key = True)
   content = db.Column(db.String(250), index=True)
 
   def __repr__(self):
     return '<Post {}>'.format(self.content)
 
-class User(db.Model):
+class User(db.Model): ## Define User Model. Contains id, username, email, password and posts list.
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
