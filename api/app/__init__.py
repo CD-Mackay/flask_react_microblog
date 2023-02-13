369f14303e9fec658db3a9a737967f7fa4aeca64
Connor Mackay <connor.mackay@gmail.com>
2023-02-13 18:38:08 -0500

    debug migration error

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 jwt = JWTManager(app)
 
 
