292d31b46becdd24830cad0b0491450304729a98
Connor Mackay <connor.mackay@gmail.com>
2023-02-16 14:03:56 -0500

    build registration form

---
+++ frontend/src/Components/RegisterForm/RegisterForm.js
@@ -0,0 +1,95 @@
+//Library Imports
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Styling Imports
+import "./RegisterForm.css";
+
+// Component Imports
+import Button from "../Button/Button";
+import UseToken from "../UseToken";
+
+const RegisterForm = () => {
+  const { saveToken } = UseToken();
+  const navigate = useNavigate();
+
+  const [RegisterForm, setRegisterForm] = useState({
+    email: "",
+    password: "",
+    username: ""
+  });
+
+  function handleRegister(event) {
+    event.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: RegisterForm.email,
+        password: RegisterForm.password,
+      }),
+    };
+
+    fetch("/register", requestOptions)
+      .then((response) => {
+        if (!response.ok) throw new Error(response.status);
+        return response.json();
+      })
+      .then((data) => {
+        saveToken(data.access_token);
+        return navigate("/");
+      })
+      .catch((error) => {
+        return console.log(error);
+      });
+
+    setRegisterForm({
+      email: "",
+      password: "",
+      username: ""
+    });
+
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setRegisterForm((prevNote) => ({
+      ...prevNote,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <form className="Register-form">
+      <input
+        onChange={handleChange}
+        type="email"
+        text={RegisterForm.email}
+        name="email"
+        placeholder="Email"
+        value={RegisterForm.email}
+      />
+      <input
+        onChange={handleChange}
+        type="text"
+        text={RegisterForm.username}
+        name="username"
+        placeholder="username"
+        value={RegisterForm.username}
+      />
+      <input
+        onChange={handleChange}
+        type="password"
+        text={RegisterForm.password}
+        name="password"
+        placeholder="Password"
+        value={RegisterForm.password}
+      />
+
+      <Button message="Register" onClick={handleRegister} />
+    </form>
+  );
+};
+
+export default RegisterForm;
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,15 @@
+// Library Imports
+import React from 'react';
+
+// Component Imports
+import RegisterForm from '../Components/RegisterForm/RegisterForm';
+
+const Register = () => {
+  return (
+    <div className="App">
+      <RegisterForm />
+    </div>
+  )
+};
+
+export default Register
