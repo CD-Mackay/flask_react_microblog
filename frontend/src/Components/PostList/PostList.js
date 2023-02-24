d7bf3b960b36047f501ea3bc1eb9c2b427e4a117
Connor Mackay <connor.mackay@gmail.com>
2023-02-24 11:35:47 -0500

    build postlist components

---
+++ frontend/src/Components/PostList/PostList.js
@@ -0,0 +1,17 @@
+//Library Imports
+import React from "react";
+
+// Styling Imports
+import "./PostList.css";
+
+// Component Imports
+
+const PostList = () => {
+  return (
+    <div>
+      <p>I am PostList</p>
+    </div>
+  );
+};
+
+export default PostList;
---
+++ frontend/src/Components/PostListItem/PostListItem.js
@@ -0,0 +1,17 @@
+//Library Imports
+import React from "react";
+
+// Styling Imports
+import "./PostListItem.css";
+
+// Component Imports
+
+const PostListItem = () => {
+  return (
+    <div>
+      <p>I am PostListItem</p>
+    </div>
+  );
+};
+
+export default PostListItem;
