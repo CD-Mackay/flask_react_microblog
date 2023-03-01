bd5ceec03fc400998f4c3bb08bde42859e2657e4
Connor Mackay <connor.mackay@gmail.com>
2023-03-01 17:31:57 -0500

    Add dummy data, map over in PostList component

--- frontend/src/Components/PostList/PostList.js
+++ frontend/src/Components/PostList/PostList.js
@@ -1,16 +1,24 @@
 //Library Imports
 import React from "react";
+import PostListItem from "../PostListItem/PostListItem";
 
 // Styling Imports
 import "./PostList.css";
 
 // Component Imports
 
-const PostList = ({posts}) => {
+const PostList = ({posts, dPosts}) => {
   console.log(posts)
+
+  const showPosts = (array) => {
+    array.map((element) => {
+      return <PostListItem author={element.author} content={element.content} title={element.title} />
+    })
+  }
   return (
     <div>
       <p>{posts}</p>
+      <p>{showPosts(dPosts)}</p>
     </div>
   );
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,15 +7,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PostList from "../Components/PostList/PostList";
 
-
 const Home = ({ token, showLogin, time, posts }) => {
+
+  const dummyPosts = [
+    {
+      title: "post1",
+      content: "postcontent",
+      author: "somegiuy",
+    },
+    {
+      title: "post1",
+      content: "postcontent",
+      author: "somegiuy",
+    },
+    {
+      title: "post1",
+      content: "postcontent",
+      author: "somegiuy",
+    },
+  ];
   return (
     <div className="App">
       <p>{time}</p>
 
-      {token && token !== "" && token !== undefined && token !== null && (
-        <PostList posts={posts} />
-      )}
+      {/* {token && token !== "" && token !== undefined && token !== null && ( */}
+      <PostList posts={posts} dPosts={dummyPosts} />
+      {/* )} */}
 
       {!showLogin && !token && (
         <Link to="/login">
