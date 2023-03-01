18dd442ef35e34bc9bbf112b9d7a87c8e16a6567
Connor Mackay <connor.mackay@gmail.com>
2023-03-01 17:33:30 -0500

    build postlistitem component

--- frontend/src/Components/PostListItem/PostListItem.js
+++ frontend/src/Components/PostListItem/PostListItem.js
@@ -6,10 +6,12 @@ import "./PostListItem.css";
 
 // Component Imports
 
-const PostListItem = () => {
+const PostListItem = ({author, content, title}) => {
   return (
     <div>
-      <p>I am PostListItem</p>
+      <h6>{title}</h6>
+      <p>{content}</p>
+      <p>{author}</p>
     </div>
   );
 };
