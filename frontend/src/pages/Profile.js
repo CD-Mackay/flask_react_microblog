c9416ba37a24b0a094bded412c8191f74a6c03fa
Connor Mackay <connor.mackay@gmail.com>
2023-03-02 13:59:50 -0500

    make profile page

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,14 @@
+// Library Imports
+import React from 'react';
+
+// Component Imports
+
+const Profile = () => {
+  return (
+    <div className="App">
+      <p>I am Profilepage</p>
+    </div>
+  )
+};
+
+export default Profile
