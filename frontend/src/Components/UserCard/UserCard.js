b4e858c69db5da87d6267b295fe0f94f73bd4266
Connor Mackay <connor.mackay@gmail.com>
2023-03-02 14:03:27 -0500

    build usercard component

---
+++ frontend/src/Components/UserCard/UserCard.js
@@ -0,0 +1,19 @@
+//Library Imports
+import React from "react";
+
+// Styling Imports
+import "./UserCard.css";
+
+// Component Imports
+
+const UserCard = () => {
+
+  return (
+    <div>
+      <h4>Username</h4>
+      <p>List of Posts goes here</p>
+    </div>
+  );
+};
+
+export default UserCard;
