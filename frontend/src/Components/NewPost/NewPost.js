73efb6608929b9cc1d2a7a9a25d474fdb0e2a568
Connor Mackay <connor.mackay@gmail.com>
2023-03-07 14:36:55 -0500

    make newpost component

---
+++ frontend/src/Components/NewPost/NewPost.js
@@ -0,0 +1,16 @@
+//Library Imports
+import React from "react";
+
+// Styling Imports
+import "./NewPost.css";
+
+// Component Imports
+
+
+const NewPost = () => {
+  return (
+    <div>I am NewPost</div>
+  );
+};
+
+export default NewPost;
