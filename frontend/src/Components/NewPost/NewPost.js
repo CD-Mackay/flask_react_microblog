4706c2290f28fd5a8f2d62bd02bfab21f67cfd05
Connor Mackay <connor.mackay@gmail.com>
2023-03-07 14:41:05 -0500

    add state and handlechange logic

--- frontend/src/Components/NewPost/NewPost.js
+++ frontend/src/Components/NewPost/NewPost.js
@@ -1,5 +1,5 @@
 //Library Imports
-import React from "react";
+import React, { useState } from "react";
 
 // Styling Imports
 import "./NewPost.css";
@@ -8,10 +8,29 @@ import "./NewPost.css";
 
 
 const NewPost = () => {
+
+  const [postForm, setPostForm] = useState({
+    title: "",
+    content: ""
+  })
+
+  const handleNewPost = (event) => {
+    event.preventDefault()
+
+  }
+
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setPostForm((prevNote) => ({
+      ...prevNote,
+      [name]: value,
+    }));
+  }
   return (
     <form className="new-post">
-      <input type="text" placeholder="Title" />
-      <textarea />
+      <input onChange={handleChange} type="text" placeholder="Title" />
+      <textarea onChange={handleChange} />
     </form>
   );
 };
