5f681cc6ee7df5d343de44bb5f437062d4d8489c
Connor Mackay <connor.mackay@gmail.com>
2023-03-09 13:37:04 -0500

    clean up code, remove unused import and organize css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,7 +4,6 @@ import React from "react";
 // Style Imports
 
 // Component Imports
-import { Link } from "react-router-dom";
 import NewPost from "../Components/NewPost/NewPost";
 import PostList from "../Components/PostList/PostList";
 
