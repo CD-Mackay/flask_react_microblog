30d6f67913d9f11fc4d320f7944a0e9f4ff7da7d
Connor Mackay <connor.mackay@gmail.com>
2023-03-22 16:31:25 -0400

    remove length limit from password hash

--- api/app/models.py
+++ api/app/models.py
@@ -14,7 +14,7 @@ class User(db.Model): ## Define User Model. Contains id, username, email, passwo
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
