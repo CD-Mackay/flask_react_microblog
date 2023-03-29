bb9f5521338a70197e3789730c707be52aff6d0b
Connor Mackay <connor.mackay@gmail.com>
2023-03-29 18:56:02 -0400

    add logic to only show posts by user on profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -61,7 +61,7 @@ function App() {
         />
         <Route path="/login" element={<Login saveToken={saveToken} />} />
         <Route path="/register" element={<Register saveToken={saveToken} />} />
-        <Route path="/user/:id" element={<Profile />} />
+        <Route path="/user/:id" element={<Profile posts={posts} />} />
       </Routes>
       <Footer />
     </BrowserRouter>
