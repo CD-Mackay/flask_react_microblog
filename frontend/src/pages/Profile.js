1ac25eb37edbba5c13ae348f7a4c158238b773d6
Connor Mackay <connor.mackay@gmail.com>
2023-03-31 14:56:18 -0400

    implement fetch request for user profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,21 +1,39 @@
 // Library Imports
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 // Component Imports
 import UserCard from '../Components/UserCard/UserCard';
 import PostList from '../Components/PostList/PostList';
+import UseToken from '../Components/UseToken';
 
 const Profile = ({posts}) => {
 
+  const { token } = UseToken()
   const location = useLocation()
   const profileId = Number(location.pathname.slice(6))
+  const [profile, setProfile] = useState()
 
   const showUserPosts = () => {
     let userPosts = posts
     .filter((post) => post.user_id === profileId)
     return <PostList posts={userPosts} />
   }
+
+  useEffect(() => {
+    fetch("/profile", {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setProfile({
+        username: data.username,
+        id:data.id,
+        posts: data.posts
+      }));
+
+  }, [])
   return (
     <div className="App">
       <p>I am Profilepage</p>
