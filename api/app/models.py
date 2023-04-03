c2c305738417a7290f7a654dd387a2d2a4371aea
Connor Mackay <connor.mackay@gmail.com>
2023-04-03 16:03:29 -0400

    Add followed column to User model

--- api/app/models.py
+++ api/app/models.py
@@ -30,6 +30,11 @@ class User(db.Model): ## Define User Model. Contains id, username, email, passwo
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+      'User', secondary=followers,
+      primaryjoin=(followers.c.follower_id == id),
+      secondaryjoin=(followers.c.followed_id == id),
+      backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
