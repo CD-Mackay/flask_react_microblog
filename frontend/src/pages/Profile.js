0b831df4292f0401303d2028b6a1a49b6d4e04a9
Connor Mackay <connor.mackay@gmail.com>
2023-04-11 17:33:49 -0400

    refactor profile fetch

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,48 +1,49 @@
 // Library Imports
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 // Component Imports
-import UserCard from '../Components/UserCard/UserCard';
-import PostList from '../Components/PostList/PostList';
-import UseToken from '../Components/UseToken';
+import UserCard from "../Components/UserCard/UserCard";
+import PostList from "../Components/PostList/PostList";
+import UseToken from "../Components/UseToken";
 
-const Profile = ({posts, userProfile}) => {
-
-  const { token } = UseToken()
-  const location = useLocation()
-  const profileId = Number(location.pathname.slice(6))
+const Profile = ({ posts, userProfile }) => {
+  const { token } = UseToken();
+  const location = useLocation();
+  const profileId = Number(location.pathname.slice(6));
   const [profile, setProfile] = useState({
     username: "",
     id: "",
   });
 
   const showUserPosts = () => {
-    let userPosts = posts
-    .filter((post) => post.user_id === profileId)
-    return <PostList posts={userPosts} />
-  }
+    let userPosts = posts.filter((post) => post.user_id === profileId);
+    return <PostList posts={userPosts} />;
+  };
 
   useEffect(() => {
-    fetch(`/profile/${profileId}`, {
-      headers: {
-        Authorization: "Bearer " + token,
-      }
-    })
-      .then((res) => res.json())
-      .then((data) => setProfile({
+    async function fetchProfile(profileId, token) {
+      const response = await fetch(`/profile/${profileId}`, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      const data = await response.json();
+      return {
         username: data.username,
-        id:data.id
-      }));
+        id: data.id,
+      };
+    }
 
-  }, [])
+    fetchProfile(profileId, token).then((profile) => setProfile(profile));
+  }, []);
   return (
     <div className="App">
       <p>I am Profilepage</p>
       {posts && !posts.msg && showUserPosts()}
       <UserCard profile={profile} />
     </div>
-  )
+  );
 };
 
-export default Profile
+export default Profile;
