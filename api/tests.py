009718aa570eb45b55e4811a0834c312f0dedb10
Connor Mackay <connor.mackay@gmail.com>
2023-04-12 12:39:31 -0400

    implement password hash test

--- api/tests.py
+++ api/tests.py
@@ -11,5 +11,16 @@ class UserModelCase(unittest.TestCase):
         self.app_context = app.app_context()
         self.app_context.push()
         db.create_all()
-        
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_hashing(self):
+        u = User(username='susan')
+        u.set_password('cat')
+        self.assertFalse(u.check_password('dog'))
+        self.assertTrue(u.check_password('cat'))
+
         
