d3a85e66a0a92a6796509ff431eec0e261a10f0a
Connor Mackay <connor.mackay@gmail.com>
2023-04-13 20:42:51 -0400

    Add error handling message to loginform

--- frontend/src/Components/LoginForm/LoginForm.js
+++ frontend/src/Components/LoginForm/LoginForm.js
@@ -9,85 +9,92 @@ import "./LoginForm.css";
 import Button from "../Button/Button";
 import UseToken from "../UseToken";
 import GetUser from "../GetUser";
+import ShowError from "../ShowError/ShowError";
 
-const LoginForm = (
+const LoginForm = () =>
   // {setUser}
-  ) => {
-  const { saveToken } = UseToken();
-  const { saveUser } = GetUser();
-  const navigate = useNavigate();
+  {
+    const { saveToken } = UseToken();
+    const { saveUser } = GetUser();
+    const navigate = useNavigate();
 
-  const [loginForm, setLoginForm] = useState({
-    email: "",
-    password: "",
-  });
-
-  function handleLogin(event) {
-    event.preventDefault();
-    console.log(event);
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        Accept: "application.json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: loginForm.email,
-        password: loginForm.password,
-      }),
-    };
-    fetch("/token", requestOptions)
-      .then((response) => {
-        console.log(response);
-        if (!response.ok) throw new Error(response.status);
-        return response.json();
-      })
-      .then((data) => {
-        console.log("login data", data, data.user)
-        saveToken(data.access_token);
-        saveUser(data.user)
-        return navigate("/");
-      })
-      .catch((error) => {
-        return console.log(error);
-      });
-
-    setLoginForm({
+    const [errorMessage, setErrorMessage] = useState("");
+    const [loginForm, setLoginForm] = useState({
       email: "",
       password: "",
     });
-  }
 
-  function handleChange(event) {
-    const { value, name } = event.target;
-    setLoginForm((prevNote) => ({
-      ...prevNote,
-      [name]: value,
-    }));
-  }
+    function handleLogin(event) {
+      event.preventDefault();
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          Accept: "application.json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: loginForm.email,
+          password: loginForm.password,
+        }),
+      };
+      fetch("/token", requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            setErrorMessage("Wrong username or password");
+            setTimeout(() => {
+              setErrorMessage("");
+            }, 2000);
+            throw new Error(response.status);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("login data", data, data.user);
+          saveToken(data.access_token);
+          saveUser(data.user);
+          return navigate("/");
+        })
+        .catch((error) => {
+          return console.log("loginError:", error);
+        });
 
-  return (
-    <form className="login-form">
-      <input
-        onChange={handleChange}
-        type="email"
-        text={loginForm.email}
-        name="email"
-        placeholder="Email"
-        value={loginForm.email}
-      />
-      <input
-        onChange={handleChange}
-        type="password"
-        text={loginForm.password}
-        name="password"
-        placeholder="Password"
-        value={loginForm.password}
-      />
+      setLoginForm({
+        email: "",
+        password: "",
+      });
+    }
 
-      <Button message="Login" onClick={(event) => handleLogin(event)} />
-    </form>
-  );
-};
+    function handleChange(event) {
+      const { value, name } = event.target;
+      setLoginForm((prevNote) => ({
+        ...prevNote,
+        [name]: value,
+      }));
+    }
+
+    return (
+      <form className="login-form">
+        <input
+          onChange={handleChange}
+          type="email"
+          text={loginForm.email}
+          name="email"
+          placeholder="Email"
+          value={loginForm.email}
+        />
+        <input
+          onChange={handleChange}
+          type="password"
+          text={loginForm.password}
+          name="password"
+          placeholder="Password"
+          value={loginForm.password}
+        />
+
+        <Button message="Login" onClick={(event) => handleLogin(event)} />
+        <ShowError message={errorMessage} />
+      </form>
+    );
+  };
 
 export default LoginForm;
