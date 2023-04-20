978e34428cccc8e2e74f67498c27a1fde41ab4b0
Connor Mackay <connor.mackay@gmail.com>
2023-04-20 15:36:32 -0400

    Add nav to explore page

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -9,25 +9,31 @@ import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import GetUser from "../GetUser";
 
-const Header = ({ token, removeToken, userProfile}) => {
-
+const Header = ({ token, removeToken, userProfile }) => {
   // const { id } = user;
   const { removeUser, user } = GetUser();
 
   const handleLogout = () => {
     removeToken();
     removeUser();
-  }
+  };
 
   return (
     <nav className="header-nav">
-      <div><p>Welcome {user && userProfile.username}!</p></div>
+      <div>
+        <p>Welcome {user && userProfile.username}!</p>
+      </div>
       <div className="button-wrapper">
         {token && <Button onClick={handleLogout} message="Logout" />}
         {token && (
-          <Link to={`/user/${user && user}`}>
-            <Button message="Profile" />
-          </Link>
+          <>
+            <Link to={`/user/${user && user}`}>
+              <Button message="Profile" />
+            </Link>
+            <Link to="/explore">
+              <Button message="Explore" />
+            </Link>
+          </>
         )}
         {!token && (
           <>
