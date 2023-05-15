5585f25071b2d9be4e742978b29e8324e8bfd303
Connor Mackay <connor.mackay@gmail.com>
2023-05-15 13:50:08 -0400

    remove unused code from app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 // Component Imports
 import UseToken from "./Components/UseToken";
-import Header from "./Components/Header/Header";
 import Home from "./Pages/Home.js";
 import Login from "./Pages/Login.js";
 import Register from "./Pages/Register";
-import Footer from "./Components/Footer/Footer";
 import Explore from "./Pages/Explore";
 import GetUser from "./Components/GetUser";
 
 //Library Imports
-import { useState, useEffect, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 
 //Styling Imports
@@ -20,35 +18,13 @@ import { UserContext } from "./Contexts/UserContext";
 function App() {
   const { token, saveToken } = UseToken();
   const { user } = GetUser();
-  const [time, setTime] = useState(0);
 
-  const { userProfile, fetchUserProfile } = useContext(UserContext);
+  const { fetchUserProfile } = useContext(UserContext);
 
-  // const [posts, setPosts] = useState("");
-  // const [followedPosts, setFollowedPosts] = useState("");
-  // const [userProfile, setUserProfile] = useState({
-  //   username: "",
-  //   id: 0,
-  //   posts: [],
-  // });
-
-  // const fetchUserProfile = async (user, token) => {
-  //   if (!user) return;
-  //   const res = await fetch(`/profile/${user}/${user}`, {
-  //     headers: {
-  //       Authorization: "Bearer " + token,
-  //     },
-  //   });
-  //   const data = await res.json();
-  //   setUserProfile({
-  //     username: data.username,
-  //     id: data.id,
-  //   });
-  // };
 
   useEffect(() => {
     fetchUserProfile(user, token); // Put this into context?
-  }, [token, user]);
+  }, [token, user, fetchUserProfile]);
 
   return (
     <>
