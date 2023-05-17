faa9c0188435d02b48cdd1a2d5309430be9ac194
Connor Mackay <connor.mackay@gmail.com>
2023-05-17 14:21:27 -0400

    style postlistitem

--- frontend/src/Components/PostListItem/PostListItem.js
+++ frontend/src/Components/PostListItem/PostListItem.js
@@ -12,8 +12,8 @@ const PostListItem = ({ user_id, content, title, author }) => {
     <div className="post-item" data-testid="post-item">
       <div className="title-card">
         <h4 className="post-title">{title}</h4>
-        <Link to={`/user/${user_id}`}>
-          <p>{author}</p>
+        <Link to={`/user/${user_id}`} className="post-link">
+          {author}
         </Link>
       </div>
       <p>{content}</p>
