048ce3a9a7cb6f3acdaa6ad05351777b7b303d07
Connor Mackay <connor.mackay@gmail.com>
2023-05-18 12:50:58 -0400

    add error handling to new name input

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -8,6 +8,7 @@ import PostList from "../Components/PostList/PostList";
 import UseToken from "../Components/UseToken";
 import GetUser from "../Components/GetUser";
 import Button from "../Components/Button/Button";
+import ShowError from "../Components/ShowError/ShowError";
 const Profile = () => {
   const { token } = UseToken();
   const { user } = GetUser();
@@ -20,18 +21,28 @@ const Profile = () => {
     followed: null,
   });
   const [nameChange, setNameChange] = useState("");
+  const [message, setMessage] = useState("");
 
   const changeUserName = async (user, newName, token) => {
-    const res = await fetch(`/change_username/${user}/${newName}`, {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    });
-    const data = await res.json();
-    console.log(data);
+    try {
+      const res = await fetch(`/change_username/${user}/${newName}`, {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      const data = await res.json();
+      setMessage(data.message);
+      setNameChange("")
+      setTimeout(() => {
+        setMessage("")
+      }, 2000)
+    } catch(error) {
+      console.log(error);
+    }
+    
   }
-  
+
   const userPosts = posts
     ? posts.filter((post) => post.user_id === profileId)
     : [];
@@ -88,6 +99,7 @@ const Profile = () => {
             />
           </form>
         )}
+        <ShowError message={message} />
       </div>
       <div className="profile-posts-wrapper">
         <p>posts by {profile.username}</p>
